8815b8f60f9c6584335eac6dfb1b84c48441a70b
Takoua Jelassi <takoua.jelassi@aislab.de>
2026-04-22 13:37:06 +0200

    on render testen

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -9,8 +9,8 @@ const GUEST_LOGINS = {
     }
 }
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-const STATIC_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = 'https://coderr-frontend-zxjc.onrender.com';
+const STATIC_BASE_URL = 'https://coderr-frontend-zxjc.onrender.com';
 
 
 const LOGIN_URL = 'login/';
