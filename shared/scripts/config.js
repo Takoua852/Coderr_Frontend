93282b44d832c23ad13e686b6ee037957162b386
Takoua Jelassi <takoua.jelassi@aislab.de>
2026-04-22 14:01:28 +0200

    url updated

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -9,8 +9,8 @@ const GUEST_LOGINS = {
     }
 }
 
-const API_BASE_URL = 'https://coderr-frontend-zxjc.onrender.com/';
-const STATIC_BASE_URL = 'https://coderr-frontend-zxjc.onrender.com/static/';
+const API_BASE_URL = 'https://coderr-frontend-zxjc.onrender.com/api/';
+const STATIC_BASE_URL = 'https://coderr-frontend-zxjc.onrender.com/';
 
 
 const LOGIN_URL = 'login/';
