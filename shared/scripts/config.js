e61fc1e268ed80b253302217dbec518cfadeccf1
Takoua Jelassi <takoua.jelassi@aislab.de>
2026-04-30 11:10:13 +0200

    config.js urls updated

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -15,25 +15,25 @@ const STATIC_BASE_URL = 'https://coderr-backend-9bjg.onrender.com/';
 
 const LOGIN_URL = 'login/';
 
-const REGISTER_URL = 'registration/';
+const REGISTER_URL = 'https://coderr-backend-9bjg.onrender.com/registration/';
 
-const PROFILE_URL = 'profile/';
+const PROFILE_URL = 'https://coderr-backend-9bjg.onrender.com/profile/';
 
-const BUSINESS_PROFILES_URL = 'profiles/business/';
+const BUSINESS_PROFILES_URL = 'https://coderr-backend-9bjg.onrender.com/profiles/business/';
 
-const CUSTOMER_PROFILES_URL = 'profiles/customer/';
+const CUSTOMER_PROFILES_URL = 'https://coderr-backend-9bjg.onrender.com/profiles/customer/';
 
-const REVIEW_URL = 'reviews/';
+const REVIEW_URL = 'https://coderr-backend-9bjg.onrender.com/reviews/';
 
-const ORDER_URL = 'orders/';
+const ORDER_URL = 'https://coderr-backend-9bjg.onrender.com/orders/';
 
-const OFFER_URL = 'offers/';
+const OFFER_URL = 'https://coderr-backend-9bjg.onrender.com/offers/';
 
-const OFFER_DETAIL_URL = 'offerdetails/';
+const OFFER_DETAIL_URL = 'https://coderr-backend-9bjg.onrender.com/offerdetails/';
 
-const BASE_INFO_URL = 'base-info/';
+const BASE_INFO_URL = 'https://coderr-backend-9bjg.onrender.com/base-info/';
 
-const OFFER_INPROGRESS_COUNT_URL = 'order-count/';
-const OFFER_COMPLETED_COUNT_URL = 'completed-order-count/';
+const OFFER_INPROGRESS_COUNT_URL = 'https://coderr-backend-9bjg.onrender.com/order-count/';
+const OFFER_COMPLETED_COUNT_URL = 'https://coderr-backend-9bjg.onrender.com/completed-order-count/';
 
 const PAGE_SIZE = 6
